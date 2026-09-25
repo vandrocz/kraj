ed8c97bad8ccb39fe19a43337f4c70de6ed3a828
vandrocz <vandrocz.contact@gmail.com>
2026-09-25 18:41:17 +0200

    Update Google Client ID and reCAPTCHA site key

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 const API_BASE_URL = 'https://naskraj-api.vandrocz-contact.workers.dev';
 const MAP_ORIGIN = 'https://maps.vandro.cz';
-const GOOGLE_CLIENT_ID = 'SEM_VLOZ_SVOJ_GOOGLE_CLIENT_ID.apps.googleusercontent.com';
-const RECAPTCHA_SITE_KEY = '';
+const GOOGLE_CLIENT_ID = '769764675952-1nb44qbpc4o7a1l5r14vt8phamom9209.apps.googleusercontent.com';
+const RECAPTCHA_SITE_KEY = '6LeXt8UtAAAAAOOSYTkbrpC4gaotPw9-SUPEuvDo';
 
 function getToken() { return localStorage.getItem('naskraj_token'); }
 function setToken(t) { localStorage.setItem('naskraj_token', t); }
